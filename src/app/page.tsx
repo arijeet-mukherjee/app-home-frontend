3f6120a1b18ffc63af246d3a1343ed67774f197a
aditya9940 <adityasingh@Adityas-MacBook-Air.local>
2024-05-13 01:05:08 +0530

    3ndcommit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,41 @@
 import Image from "next/image";
 import Hero from "@component/Hero";
 import Shield from "@component/common/Shield";
+import CardBox from "@component/cardBox";
+import data from '../data.json'
 export default function Home() {
   return (
     <div className={"container"}>
-      <Hero introduction={["Internet can be a dangerous place,", "Do you sometimes worry"]} content={["About the online scams?", "We hear these worries a lot from our clients, parents, teenagers, and friends outside IT industry, we could go on and on. Security is difficult and people from all layers of security struggle with these questions."]}/>     
-      <Shield top={95} left={1300}/>
+      <Hero introduction={["Internet can be a dangerous place,", "Do you sometimes worry"]} content={["About the online scams?", "We hear these worries a lot from our clients, parents, teenagers, and friends outside IT industry, we could go on and on. Security is difficult and people from all layers of security struggle with these questions."]} />
+      <Shield top={95} left={1300} />
+      <div style={{ padding: "160px", transform: "translateY(-50%)" }}>
+        <CardBox
+          title={data.introduction.title}
+          description={data.introduction.description}
+          image={data.introduction.image}
+          iconPosition={data.introduction.image_position}
+          buttonText={data.introduction.button_name}
+          buttonIcon={data.introduction.button_icon}
+        />
+      </div>
+      <div style={{ padding: "160px", display: "block", gap: "60px", transform: "translateY(-50%)" }}>
+        <CardBox
+          title={data.yourShield.title}
+          description={data.yourShield.description}
+          image={data.yourShield.image}
+          iconPosition={data.yourShield.image_position}
+          buttonText={data.yourShield.button_name}
+          buttonIcon={data.yourShield.button_icon}
+        />
+        <CardBox
+          title={data.jointheMovement.title}
+          description={data.jointheMovement.description}
+          image={data.jointheMovement.image}
+          iconPosition={data.jointheMovement.image_position}
+          buttonText={data.jointheMovement.button_name}
+          buttonIcon={data.jointheMovement.button_icon}
+        />
+      </div>
     </div>
   );
 }
--- src/component/cardBox/index.tsx
+++ src/component/cardBox/index.tsx
@@ -1,10 +1,9 @@
 'use client'
 import React, { useEffect } from 'react';
-import styles from './styles.module.css'; // Import CSS module for styling
+import styles from './styles.module.css';
 import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
-
 interface CardBoxProps {
     image: string,
     title: string,
@@ -15,37 +14,53 @@ interface CardBoxProps {
 };
 
 const CardBox: React.FC<CardBoxProps> = (props: CardBoxProps) => {
-    
+
     const [isMobileDevice, setIsMobileDevice] = useState<Boolean>(false);
     const [cardContentStyle, setCardContentStyle] = useState({});
     const [cardImageStyle, setCardImageStyle] = useState({});
-    
+    const [cardGradient, setCardGradient] = useState({});
 
-    useEffect(()=>{
+    const handleResize = () => {
         const isMobile = () => {
-            return typeof window !== 'undefined' && window.matchMedia("(max-width: 767px)").matches; 
+            return typeof window !== 'undefined' && window.matchMedia("(max-width: 767px)").matches;
+        }
+        const check = isMobile();
+
+        if (check === true) {
+            setCardContentStyle({ gridRow: 2, gridColumn: 1 });
+            setCardImageStyle({ gridRow: 1, gridColumn: 1, background: "linear-gradient(108.31deg, #FFFFFF 34.81%, #C7D5E0 78.11%)" });
+        } else {
+            setCardContentStyle({ gridColumn: props.iconPosition == "right" ? 1 : 2, gridRow: 1 });
+            setCardImageStyle({ gridColumn: props.iconPosition == "right" ? 2 : 1, gridRow: 1 });
+            setCardGradient({ background: props.iconPosition == "left" ? "linear-gradient(270.59deg, #FFFFFF 35.89%, #C7D5E0 79.41%)" : "linear-gradient(108.31deg, #FFFFFF 34.81%, #C7D5E0 78.11%)", gridRow: 1 });
+        }
+    }
+
+    useEffect(() => {
+
+        window.addEventListener("resize", handleResize, false);
+        const isMobile = () => {
+            return typeof window !== 'undefined' && window.matchMedia("(max-width: 767px)").matches;
         }
 
         const check = isMobile();
 
-        if(check === true) {
-            setCardContentStyle({ gridRow: 2, gridColumn: 1});
-            setCardImageStyle({gridRow: 1 , gridColumn: 1});
-            console.log("in true block")
+        if (check === true) {
+            setCardContentStyle({ gridRow: 2, gridColumn: 1 });
+            setCardImageStyle({ gridRow: 1, gridColumn: 1, background: "linear-gradient(108.31deg, #FFFFFF 34.81%, #C7D5E0 78.11%)" });
         } else {
-            setCardContentStyle({gridColumn : props.iconPosition == "left" ? 1 : 2 , gridRow: 1});
-            setCardImageStyle({gridColumn : props.iconPosition == "left" ? 2 : 1, gridRow: 1});
-            console.log("in else block")
+            setCardContentStyle({ gridColumn: props.iconPosition == "right" ? 1 : 2, gridRow: 1 });
+            setCardImageStyle({ gridColumn: props.iconPosition == "right" ? 2 : 1, gridRow: 1 });
+            setCardGradient({ background: props.iconPosition == "left" ? "linear-gradient(270.59deg, #FFFFFF 35.89%, #C7D5E0 79.41%)" : "linear-gradient(108.31deg, #FFFFFF 34.81%, #C7D5E0 78.11%)", gridRow: 1 });
         }
-    },[])
-
-    //const isMobileDevice = isMobile();
-    
+    }, [])
 
     const { image, title, description, iconPosition, buttonText } = props;
+    const imageWidth = 'calc((100vw / 1920) * 329.61'
+    const imageHeight = 'calc((100vw / 1920) * 307.29'
     return (
-        <div className={styles["cardbox-card"]}  >
-            <div className={styles["cardbox-content"]} style = {cardContentStyle} >
+        <div className={styles["cardbox-card"]} style={cardGradient} >
+            <div className={styles["cardbox-content"]} style={cardContentStyle} >
                 <h2 className={styles["cardbox-title"]}>{title}</h2>
                 <p className={styles["cardbox-description"]}>{description}</p>
                 <div className={styles["cardbox-button"]}>
@@ -59,20 +74,11 @@ const CardBox: React.FC<CardBoxProps> = (props: CardBoxProps) => {
                     </Link>
                 </div>
             </div>
-            <div className={styles["cardbox-image"]} style = {cardImageStyle}>
+            <div className={styles["cardbox-image"]} style={cardImageStyle}>
                 <Image src={image} alt={title} width={329.61} height={307.29} />
             </div>
         </div>
     );
 };
-//// style={{ gridColumnStart: iconPosition === "left" ? 2 : 1 }}
-{/* <CardBox
-      title="Introduction"
-      description="Welcome to SecDesk@Home - your shield against cybercriminals in an increasingly digitized world. As technology advances, so do the threats, and SecDesk couldn't stand idly by any longer. We've witnessed too many hard-earned dollars fall into the hands of cybercriminals, and enough is enough. It's time for a solution designed to protect individuals like you who are active online."
-      image="/introduction.svg"
-      iconPosition="left"
-      buttonText="Know More"
-      buttonIcon="/arrowrightwhite.svg"
-      /> */}
 
 export default CardBox;
