9f3d672f10b30128b9e1a996cfcb8a1337b40e38
Bibhuti <140835061+Bibhuti05@users.noreply.github.com>
2024-05-31 11:17:47 +0530

    reverted common button to OG

--- src/component/common/Button/index.tsx
+++ src/component/common/Button/index.tsx
@@ -1,36 +1,22 @@
 'use client';
 import Image from 'next/image';
 import styles from './styles.module.css';
-import { useRef, useEffect, useState } from "react";
-import { isMobile } from '@util/index';
+import { useRef, useEffect } from "react";
+
 
 interface ButtonProps {
   label: string;
-  buttonColor?: string;
-  transform?: string;
   action_svg: string;
   svg_height?: number;
   svg_width?: number;
   hc?: Function;
-
 }
 
-const Button: React.FC<ButtonProps> = ({ label, 
-  buttonColor = "var(--text-primary-color)", 
-  transform = "translateX(0) translateY(0)",
-  action_svg, svg_height = 18, 
-  svg_width = 18, 
-  hc,
-}) => {
+const Button: React.FC<ButtonProps> = ({ label, action_svg, svg_height = 18, svg_width = 18, hc}) => {
 
 
   const elementRef = useRef<HTMLButtonElement>(null);
-  const [isMobileDevice, setIsMobileDevice] = useState(false);
-
-  let action_svg_width_style = isMobileDevice? 'calc((100vw / 393)* 1920)' : `calc((100vw/1920)* ${svg_width}`;
-
   useEffect(() => {
-    setIsMobileDevice(isMobile());
     function handleResize() {
       let rect = elementRef?.current?.getBoundingClientRect();
       //read the position of the button from the localStorage with key 'nav_bar_button_coordinates
@@ -43,14 +29,7 @@ const Button: React.FC<ButtonProps> = ({ label,
 
   return (
     <>
-      <button className={styles.button} 
-      ref={elementRef} 
-      aria-label={`${label} button`} 
-      style={{
-        backgroundColor: `${buttonColor}`,
-        transform: `${transform}`,
-      }}
-      onClick={(e) =>{
+      <button className={styles.button} ref={elementRef} aria-label={`${label} button` } onClick={(e) =>{
         if(hc) hc(e);
       }}>
         {label}
@@ -59,10 +38,6 @@ const Button: React.FC<ButtonProps> = ({ label,
           alt={`${label} icon`}
           width={svg_width}
           height={svg_height}
-          style={{
-            width: `calc((100vw/1920)* ${svg_width}`,
-            height: 'auto',
-          }}
         />
       </button>
 
