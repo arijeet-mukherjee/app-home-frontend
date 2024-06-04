987effa3ceb8bff4b6a5128f3cf066ab96f89b23
Sarvesh025 <sarveshkumarroy1@gmail.com>
2024-06-04 22:13:31 +0530

    Implimented coupon in inbox, moved the emailVarification logic to util.tsx, Created animation for coupon in the common folder.

--- src/utils/util/index.tsx
+++ src/utils/util/index.tsx
@@ -23,6 +23,16 @@ export function goToTop() {
     });
 }
 
+export function emailVerified(email: string): boolean {
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!emailRegex.test(email)) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
 export function makeWebServiceCall(url: string, method: string, data: any): Promise<any> {
     // Implement your web service call logic here
     // You can use libraries like axios or fetch to make the actual HTTP request
