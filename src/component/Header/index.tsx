94473e052437c45ba2bf374dd2dfa74a585316a4
Ayush Kedia <ayushkedia1990@gmail.com>
2024-06-12 23:02:02 +0530

    merge conflicts

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -4,23 +4,24 @@ import Image from 'next/image';
 import styles from "./styles.module.css";
 import Button from '../common/Button';
 import Link from 'next/link';
-import data from '../../data.json';
+import { useAppSelector } from '@store/store';
 import DDMenu from '@component/common/DropdownMenu';
 import { isMobile } from '@util/index';
 
 
 interface HeaderProps {
     openModal: Function;
-    modalState: boolean
+    modalState: boolean;
+    headerData: any;
 };
 
-const Header: React.FC<HeaderProps> = ({ openModal, modalState }) => {
+const Header: React.FC<HeaderProps> = ({ openModal, modalState, headerData }) => {
     const [logoSize, setLogoSize] = useState({
         height: 48,
         width: 176
     })
     const [open, setOpen] = useState(false);
-
+    const globalLanguage = useAppSelector<any>(state => state.globalLanguage);
     const handledd: Function = () => {
         setOpen(!open)
     }
@@ -44,10 +45,10 @@ const Header: React.FC<HeaderProps> = ({ openModal, modalState }) => {
     return (
         <nav className={styles.header}>
             <div >
-                <Link href={data.header.navigation_bar.logo.href}>
+                <Link href={headerData.navigation_bar.logo.href}>
                     <Image
-                        src={data.header.navigation_bar.logo.src}
-                        alt={data.header.navigation_bar.logo.alt}
+                        src={headerData.navigation_bar.logo.src}
+                        alt={headerData.navigation_bar.logo.alt}
                         width={logoSize.width}
                         height={logoSize.height}
                         className={styles.logo}
@@ -55,7 +56,7 @@ const Header: React.FC<HeaderProps> = ({ openModal, modalState }) => {
                 </Link>
             </div>
             <div className={styles.navbar}>
-                {data.header.navigation_bar.navbarItems?.map((item, index) => {
+                {headerData.navigation_bar.navbarItems?.map((item: any, index: any) => {
                     if (item.dditem?.length === 0) {
                         return (
                             <div aria-label={item.label} key={index}>
@@ -71,7 +72,7 @@ const Header: React.FC<HeaderProps> = ({ openModal, modalState }) => {
                                 handledd();
                             }}
                             style={{ color: 'white' }}
-                        >{item.label}
+                        >{globalLanguage.globalLanguage}
                             <span className={styles.arrowDown} style={{
                                 transform: open ? 'rotate(180deg)' : ''
                             }}></span>
@@ -82,10 +83,10 @@ const Header: React.FC<HeaderProps> = ({ openModal, modalState }) => {
                 })
                 }
 
-                <Button label={data.header.navigation_bar.button.label} action_svg={data.header.navigation_bar.button.action_svg} hc={handelClick} />
+                <Button label={headerData.navigation_bar.button.label} action_svg={headerData.navigation_bar.button.action_svg} hc={handelClick} />
             </div>
             {modalState && <div className={styles.mobileLang}>
-                {data.header.navigation_bar.navbarItems?.map((item, index) => {
+                {headerData.navigation_bar.navbarItems?.map((item: any, index: any) => {
                     if (item.dditem?.length !== 0) {
                         return (
                             <div aria-label={item.label} key={index}
@@ -95,7 +96,7 @@ const Header: React.FC<HeaderProps> = ({ openModal, modalState }) => {
                                     handledd();
                                 }}
                                 style={{ color: 'white' }}
-                            >{item.label}
+                            >{globalLanguage.globalLanguage}
                                 <span className={styles.arrowDown} style={{
                                     transform: open ? 'rotate(180deg)' : ''
                                 }}></span>
