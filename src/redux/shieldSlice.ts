d73ca23835a1aeaa9cdc3735aac44aaf9846a042
Arijeet Mukherjee <ari123acess@gmail.com>
2024-06-15 00:20:07 +0530

    Update shieldSlice.ts

--- src/redux/shieldSlice.ts
+++ src/redux/shieldSlice.ts
@@ -8,7 +8,7 @@ export interface ShieldState {
 
 const initialState: ShieldState = {
   top: 105,
-  right: 190,
+  right: 210,
   visible: true
 };
 
@@ -17,7 +17,10 @@ export const shieldSlice = createSlice({
   initialState,
   reducers: {
     setShieldState: (state, action: PayloadAction<ShieldState>) => {
-        state = action.payload;
+      console.log('shield pa', action.payload)
+      state.top = action.payload.top;
+      state.right = action.payload.right;
+      state.visible = action.payload.visible;
     },
   },
 });
