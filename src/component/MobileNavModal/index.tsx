d171c0e1a791b2c20f792f16fa5aecf97287df5b
aditya9940 <adityasingh@Adityas-MacBook-Air.local>
2024-06-14 19:50:57 +0530

    minorfixes

--- src/component/MobileNavModal/index.tsx
+++ src/component/MobileNavModal/index.tsx
@@ -45,7 +45,7 @@ const MobileNavModal: FC<MobileNavModalProps> = ({ list, closeModal, modalState,
                     if (item.dditem?.length === 0) {
                         return (
 
-                            <Link className={styles.navItem} href={item.url} key={index} >{item.label}</Link>
+                            <p className={styles.navItem} key={index} >{item.label}</p>
 
                         )
                     }
